0889b0d21e5acb01f2e58391730fee89da084e3f
Carl Dersell <carl.dersell@leica-geosystems.com>
2024-12-26 22:28:24 +0100

    Added remaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>dl', function()
+  vim.diagnostic.open_float { scope = 'line' }
+end, { desc = 'Show diagnostics for the current line' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -50,5 +54,61 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- Move marked lines in visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move marked lines down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move marked lines up' })
+
+-- Mappping indent and outdent
+vim.keymap.set('n', '<tab>', '>>', { desc = 'Indent line' })
+vim.keymap.set('n', '<S-tab>', '<<', { desc = 'Deindent line' })
+vim.keymap.set('x', '<tab>', '>', { desc = 'Indent line' })
+vim.keymap.set('x', '<S-tab>', '<', { desc = 'Deindent line' })
+
+-- Center on jumping
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Center on jumping down' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center on jumping up' })
+vim.keymap.set('n', 'n', 'nzz', { desc = 'Center on searching' })
+vim.keymap.set('n', 'N', 'Nzz', { desc = 'Center on searching' })
+
+-- Combine lines, but keep cursor position
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Combine lines, but keep cursor position' })
+
+-- In visual mode search for marked text
+vim.keymap.set('x', '*', '"zy/<C-R>z<CR>', { desc = 'Search for marked text in visual mode' })
+
+-- Start changing word under cursor
+vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = 'Start chaning word under cursor' })
+
+-- In visual mode, replace text and keep in register
+vim.keymap.set('x', '<leader>P', '"_dP', { desc = 'Replace text and keep in register' })
+
+-- Copy to clipboard
+vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Copy to EOL to system clipboard' })
+-- nmap <leader>Y \"+Y
+
+-- Paste from clipbord
+vim.keymap.set('n', '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set('v', '<leader>p', '"_d"+P', { desc = 'Paste from system clipboard and keep register' })
+
+-- In visual mode, delete and move to clipboard
+vim.keymap.set('x', '<leader>D', '"+d', { desc = 'Delete and move to clipboard' })
+
+-- In visual mode, delete and move to unnamed register
+vim.keymap.set('x', '<leader>d', '"_d', { desc = 'Delete to unnamed register' })
+
+-- In normal mode, delete and move to unnamed register
+-- vim.keymap.set('n', '<leader>dd', '"_dd', { desc = 'Delete to unnamed register' })
+vim.keymap.set('n', '<leader>d', '"_d', { desc = 'Delete to unnamed register' })
+
+--Map to yank entire file into clipboard
+vim.keymap.set('n', '<leader>ac', '<cmd>%y+<CR>', { desc = 'Yank entire file to system clipboard' })
+
+-- Apply macro over visual range
+vim.keymap.set('x', '@', function()
+  return ':norm @ ' .. vim.fn.getcharstr() .. '<CR>'
+end, { expr = true })
 
 -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -77,8 +77,12 @@ return {
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search [G]it [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      -- vim.keymap.set('n', '<leader>ps', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
@@ -96,6 +100,7 @@ return {
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      -- vim.keymap.set('n', '<leader>sb', function()
       vim.keymap.set('n', '<leader>s/', function()
         builtin.live_grep {
           grep_open_files = true,
