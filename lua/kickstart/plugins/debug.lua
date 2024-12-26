001e3bf1e32639ecd837d417f97f5d54a116fd5a
Carl Dersell <carl.dersell@leica-geosystems.com>
2024-12-26 22:34:25 +0100

    Added some default extra plugins

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -68,6 +68,13 @@ return {
       end,
       desc = 'Debug: Set Breakpoint',
     },
+    {
+      '<F9>',
+      function()
+        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end,
+      desc = 'Debug: Set Breakpoint',
+    },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
       '<F7>',
@@ -121,16 +128,16 @@ return {
     }
 
     -- Change breakpoint icons
-    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-    -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-    -- for type, icon in pairs(breakpoint_icons) do
-    --   local tp = 'Dap' .. type
-    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-    -- end
+    vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+    vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+    local breakpoint_icons = vim.g.have_nerd_font
+        and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+    for type, icon in pairs(breakpoint_icons) do
+      local tp = 'Dap' .. type
+      local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+    end
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -16,6 +16,19 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+
+      -- Incremental selection setup with Treesitter
+      -- check out what this does!
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          -- Keymaps for incremental selection
+          init_selection = 'gnn', -- Start selection (default to 'gnn')
+          node_incremental = 'grn', -- Increment selection to the next node (default to 'grn')
+          scope_incremental = 'grc', -- Increment selection to the scope (default to 'grc')
+          node_decremental = 'grm', -- Decrement selection (default to 'grm')
+        },
+      },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
@@ -23,6 +36,36 @@ return {
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true, -- Enable this plugin (can be toggled per buffer with :TSContextToggle)
+        throttle = true, -- Throttle updates (may improve performance)
+        max_lines = 5, -- How many lines the context window should span. 0 = no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- Default patterns for all file types
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+        multiwindow = false, -- No multiwindow support
+        min_window_height = 0, -- No limit on minimum window height
+        line_numbers = true, -- Show line numbers in the context window
+        multiline_threshold = 2, -- Max number of lines to show for a single context
+        trim_scope = 'outer', -- Discard the outer context if `max_lines` is exceeded
+        mode = 'cursor', -- Use the cursor position to calculate context
+        separator = nil, -- No separator by default
+        zindex = 20, -- Set the Z-index of the context window
+        on_attach = nil, -- No custom on_attach function
+      }
+      vim.keymap.set('n', '[tc', function()
+        require('treesitter-context').go_to_context(vim.v.count1)
+      end, { silent = true, desc = 'Previous [C]ontext' })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
