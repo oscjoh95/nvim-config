bd62af927377474c95ce86fd58c05d11b7f16fc0
Carl Dersell <carl.dersell@leica-geosystems.com>
2025-05-09 14:11:09 +0200

    Can now remove entries from harpoon list

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,19 +10,30 @@ return {
       -- basic telescope configuration
       local conf = require('telescope.config').values
       local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
+        local finder = function()
+          local file_paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(file_paths, item.value)
+          end
+          return require('telescope.finders').new_table { results = file_paths }
         end
 
         require('telescope.pickers')
           .new({}, {
             prompt_title = 'Harpoon',
-            finder = require('telescope.finders').new_table {
-              results = file_paths,
-            },
+            finder = finder(),
             previewer = conf.file_previewer {},
             sorter = conf.generic_sorter {},
+            attach_mappings = function(prompt_bufnr, map)
+              map('n', '<leader>db', function()
+                local state = require 'telescope.actions.state'
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+              end)
+              return true
+            end,
           })
           :find()
       end
