2a4e764dd5f2995be937aaadb74252d8feafe2f3
Carl Dersell <carl.dersell@leica-geosystems.com>
2025-05-27 15:45:17 +0200

    Added git conflicts to status line

--- lua/custom/plugins/statusline.lua
+++ lua/custom/plugins/statusline.lua
@@ -1,3 +1,33 @@
+local function git_conflict_status()
+  local total_conflicts = 0
+  local current_conflict = 0
+  local in_conflict = false
+  local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+
+  for i = 1, vim.fn.line '$' do
+    local line = vim.fn.getline(i)
+    if line:match '^<<<<<<<' then
+      total_conflicts = total_conflicts + 1
+      in_conflict = true
+    elseif line:match '^=======' and in_conflict then
+      if i >= cursor_line then
+        current_conflict = total_conflicts
+        break
+      end
+    elseif line:match '^>>>>>>>' and in_conflict then
+      in_conflict = false
+    end
+  end
+
+  if total_conflicts == 0 then
+    return ''
+  elseif current_conflict > 0 then
+    return 'Conflict: [' .. current_conflict .. '/' .. total_conflicts .. ']'
+  else
+    return 'Conflict: [x /' .. total_conflicts .. ']'
+  end
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -47,6 +77,15 @@ return {
       },
     }
 
+    local conflicts = {
+      separator = { left = '', right = '' },
+
+      git_conflict_status,
+      cond = function()
+        return vim.bo.modifiable and vim.fn.search('<<<<<<<', 'nw') ~= 0
+      end,
+    }
+
     require('lualine').setup {
       options = {
         always_divide_middle = false,
@@ -56,8 +95,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch' },
         lualine_c = { 'filename' },
-        -- lualine_x = { 'searchcount' },
-        lualine_x = {},
+        lualine_x = { conflicts },
         lualine_y = { diff, diagnostics },
         lualine_z = { location, progress },
       },
