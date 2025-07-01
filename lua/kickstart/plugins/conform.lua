8e41a2b82a462ba64798f688c06c957213316776
Carl Dersell <carl.dersell@leica-geosystems.com>
2025-07-01 09:33:59 +0200

    Added ruff for python formatting

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -8,7 +8,7 @@ local function get_format_options(bufnr)
   else
     return {
       timeout_ms = 2000,
-      lsp_format = 'fallback',
+      lsp_format = 'never',
     }
   end
 end
@@ -16,6 +16,12 @@ end
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
+    dependencies = {
+      'mason-org/mason.nvim',
+      'jay-babu/mason-null-ls.nvim',
+      'mason-org/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
+    },
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
@@ -36,13 +42,15 @@ return {
     },
     opts = {
       notify_on_error = false,
+      default_format_opts = { lsp_format = 'never' },
+      stop_after_first = false,
       format_on_save = get_format_options,
       formatters_by_ft = {
         lua = { 'stylua' },
         cpp = { 'clang_format' },
         c = { 'clang_format' },
         markdown = { 'markdownlint' },
-        python = { 'ruff' },
+        python = { 'ruff_organize_imports', 'ruff_format', 'ruff_fix' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
